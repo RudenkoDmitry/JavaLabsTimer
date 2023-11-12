80bcb8b6ed6f2079180429c1504a914619a98140
RudenkoDmitry <103836201+RudenkoDmitry@users.noreply.github.com>
2023-11-12 21:57:34 +0300

    add table method to main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,23 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 
+
 public class Main {
+    public static void table (String result_add, String result_get, String result_delete, int k) {
+        System.out.printf ("%-10s%-10s%-10s%n", "Method" + "\t", "ArrayList" + "\t\t\t\t\t", "LinkedList");
+
+        System.out.printf("%-10s", "add");
+        System.out.printf("%15s", result_add);
+        System.out.println();
+
+        System.out.printf("%-10s", "get");
+        System.out.printf("%16s", result_get);
+        System.out.println();
+
+        System.out.printf("%-10s", "delete");
+        System.out.printf("%15s", result_delete);
+        System.out.println();
+    }
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
@@ -13,11 +29,12 @@ public class Main {
         AbstractTimer timer;
         timer = new Timer();
         timer.addElems(k);
-        System.out.println(timer);
+        String add_time = timer.toString();
         timer.getElems(k);
-        System.out.println(timer);
+        String get_time = timer.toString();
         timer.deleteElems(k);
-        System.out.println(timer);
+        String delete_time = timer.toString();
+        table(add_time,get_time,delete_time,k);
 
     }
 }
